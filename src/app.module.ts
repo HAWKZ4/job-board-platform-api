fe932596e44c44d704c52e120d3f1d2422c52060
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-06-02 21:16:36 +0300

    feat: scaffold profile module and controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ProfilesModule } from './profiles/profiles.module';
 
 @Module({
   imports: [
@@ -31,6 +32,8 @@ import { AuthModule } from './auth/auth.module';
     }),
 
     AuthModule,
+
+    ProfilesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
