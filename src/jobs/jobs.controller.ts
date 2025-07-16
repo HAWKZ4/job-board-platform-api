bb1a418b0c6f5bb80262de831ffa596af87d8cb8
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-07-16 18:53:47 +0300

    feat(jobs): implement job listing endpoint

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { JobsService } from './jobs.service';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { PaginatedResult } from 'src/common/interfaces/paginated-result.interface';
+import { Job } from './entites/job.entity';
 
 @Controller('jobs')
-export class JobsController {}
+export class JobsController {
+  constructor(private readonly jobsService: JobsService) {}
+
+  @Get()
+  async getAllJobs(
+    @Query() pagiantionDto: PaginationDto,
+  ): Promise<PaginatedResult<Job>> {
+    return this.jobsService.findAll(pagiantionDto);
+  }
+}
--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Job } from './entites/job.entity';
+import { Repository } from 'typeorm';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { PaginatedResult } from 'src/common/interfaces/paginated-result.interface';
 
 @Injectable()
-export class JobsService {}
+export class JobsService {
+  constructor(
+    @InjectRepository(Job) private readonly jobRepo: Repository<Job>,
+  ) {}
+
+  async findAll(paginationDto: PaginationDto): Promise<PaginatedResult<Job>> {
+    const page = paginationDto.page ?? 1;
+    const limit = paginationDto.limit ?? 1;
+
+    const [jobs, total] = await this.jobRepo.findAndCount({
+      skip: (page - 1) * limit,
+      take: limit,
+      order: { createdAt: 'DESC' },
+    });
+
+    return {
+      data: jobs,
+      meta: {
+        total,
+        page,
+        limit,
+      },
+    };
+  }
+}
