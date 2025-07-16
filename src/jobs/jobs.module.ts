20c050e2ac4064b170a50bf626d7360f68ce159a
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-07-16 18:48:08 +0300

    feat(jobs): register Job entity in JobsModule

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JobsService } from './jobs.service';
 import { JobsController } from './jobs.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Job } from './entites/job.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Job])],
   providers: [JobsService],
-  controllers: [JobsController]
+  controllers: [JobsController],
 })
 export class JobsModule {}
