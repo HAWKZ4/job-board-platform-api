627bfdfa64c1769f2e6e829d9ba0df141239b1ac
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-07-17 15:38:19 +0300

    chore(jbos): remove unused import jobs.controller

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -11,7 +11,6 @@ import { JobsService } from './jobs.service';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
 import { PaginatedResult } from 'src/common/interfaces/paginated-result.interface';
 import { Job } from './entites/job.entity';
-import { CreateJobDto } from './dtos/create-job.dto';
 import { PublicJobDto } from './dtos/public-job.dto';
 import { Serialize } from 'src/common/interceptors/serialize.interceptor';
 
@@ -33,6 +32,4 @@ export class JobsController {
   ): Promise<PublicJobDto> {
     return this.jobsService.findJobByIdForUser(id);
   }
-
- 
 }
