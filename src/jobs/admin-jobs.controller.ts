792e5d88b3944f4919c39b5972c18f87a8441849
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-07-18 15:42:11 +0300

    feat(jobs): protect admin endpoints with JwtAuthGuard and RolesGuard

--- src/jobs/admin-jobs.controller.ts
+++ src/jobs/admin-jobs.controller.ts
@@ -9,6 +9,7 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { JobsService } from './jobs.service';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
@@ -17,12 +18,18 @@ import { CreateJobDto } from './dtos/create-job.dto';
 import { Serialize } from 'src/common/interceptors/serialize.interceptor';
 import { AdminJobDto } from './dtos/admin-job.dto';
 import { UpdateJobDto } from './dtos/update-jobs.dto';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { UserRole } from 'src/common/enums/user-role.enum';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('admin/jobs')
 export class AdminJobsController {
   constructor(private readonly jobsService: JobsService) {}
 
   @Serialize(AdminJobDto)
+  @Roles(UserRole.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Get()
   async getAllJobs(
     @Query() pagiantionDto: PaginationDto,
@@ -33,6 +40,8 @@ export class AdminJobsController {
   }
 
   @Serialize(AdminJobDto)
+  @Roles(UserRole.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Get('/:id')
   async getJobById(
     @Param('id', ParseIntPipe) id: number,
@@ -41,12 +50,16 @@ export class AdminJobsController {
   }
 
   @Serialize(AdminJobDto)
+  @Roles(UserRole.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Post()
   async createJob(@Body() createJobDto: CreateJobDto): Promise<AdminJobDto> {
     return this.jobsService.create(createJobDto);
   }
 
   @Serialize(AdminJobDto)
+  @Roles(UserRole.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Patch('/:id')
   async updateJob(
     @Param('id', ParseIntPipe) id: number,
@@ -56,11 +69,15 @@ export class AdminJobsController {
     return updatedJob;
   }
 
+  @Roles(UserRole.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Patch('/restore/:id')
   async restoreJob(@Param('id', ParseIntPipe) id: number): Promise<void> {
     await this.jobsService.restore(id);
   }
 
+  @Roles(UserRole.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @HttpCode(204)
   @Delete('/:id')
   async deleteJob(
