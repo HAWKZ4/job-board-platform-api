ac593d8bfd8ef4c880e63fc8e5c30b9c335b3945
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-07-25 16:45:12 +0300

    chore: enable CORS for the API

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api/v1');
   const configService = app.get(ConfigService);
   app.useGlobalPipes(
